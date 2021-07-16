e1353eacff2e9fd5fa2fad64849c4d5242595b19
xdr3am <xdreamroyale@yandex.ru>
2021-07-16 15:38:39 +0300

    Добавляет блок с архивом

--- build/js/main.js
+++ build/js/main.js
@@ -1,5 +1,32 @@
 'use strict';
 
+document.addEventListener('DOMContentLoaded', () => {
+  const accordions = document.querySelectorAll('.accordion');
+
+  accordions.forEach(el => {
+    const control = el.querySelector('.accordion__control');
+    const content = el.querySelector('.accordion__content');
+
+    control.addEventListener('click', (e) => {
+      e.preventDefault();
+      el.classList.toggle('open');
+
+      // если открыт аккордеон
+      if (el.classList.contains('open')) {
+        control.setAttribute('aria-expanded', true);
+        content.setAttribute('aria-hidden', false);
+        content.style.maxHeight = content.scrollHeight + 'px';
+      } else {
+        control.setAttribute('aria-expanded', false);
+        content.setAttribute('aria-hidden', true);
+        content.style.maxHeight = null;
+      }
+    });
+  });
+});
+
+'use strict';
+
 (function () {
   const dropdowns = document.querySelectorAll('.dropdown');
 
